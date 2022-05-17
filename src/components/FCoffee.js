d8a6e2e76be98ef6f647814b81035b97d7819824
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-05-17 11:23:28 -0400

    created state and props

---
+++ src/components/FCoffee.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+function FCoffee({firstName}){
+    const [count, setCount] = useState(0);
+    const handleClick =() => {
+        setCount(count + 1);
+    }
+    const subClick =() => {
+        setCount(count - 1);
+    }
+    return(
+    <>
+        <h1> Coffee List (F)</h1>
+        <p> Hello {firstName}</p>
+        <p> You clicked the button {count} times. </p>
+        <button onClick={handleClick}>Click Up</button>
+        <button onClick={subClick}>Click Down</button>
+    </>
+    )
+}
+
+
+
+
+export default FCoffee;
